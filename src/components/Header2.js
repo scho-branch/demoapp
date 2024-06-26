21aefad32b5daa3f01686cba7e0835d3cd055fe7
Suzy Cho <suzy.cho@branch.io>
2024-06-26 12:48:09 +0900

    added gh-pages

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -63,10 +63,11 @@ const Header2 = () => {
         <header className="bg-white">
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-            </a>
+            <Link to={"/"}>
+              <div className="w-[40px]">
+                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+              </div>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
